aa9e83a45d3155b2dff969927398a6eb05a3bbc6
keistyn <khrystinfionaconejos.ramirez@bicol-u.edu.ph>
2022-07-25 09:09:54 +0800

    first commit

---
+++ app/Models/Appointment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    use HasFactory;
+    protected $table = 'appointment';
+
+    protected $dates = [
+        'start_time',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'finish_time',
+    ];
+    protected $fillable = [
+        'name',
+        'date',
+        'concern',
+    ];
+}
---
+++ resources/views/appointment.blade.php
@@ -0,0 +1,56 @@
+
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+ 
+        <!--navigation bar -->
+        <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" hhref="{{ route('login') }}">BU and bucaf logo here <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style=" margin-left:1325px; position:absolute" href="{{ route('login') }}">Login</a>
+            </li>
+
+            </ul>
+        </div>
+        </nav>
+            <!--navigation bar -->
+         
+
+        <x-jet-validation-errors class="mb-4" />
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+
+            <div>
+                <x-jet-label for="name" value="{{ __('Name') }}" />
+                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="date" value="{{ __('Date') }}" />
+                <x-jet-input id="date" class=" w-full form-controller datepicker" type="date" name="date" :value="old('date')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="concern" value="{{ __('Concern') }}" />
+                <x-jet-input id="concern" class="block mt-1 w-full" type="text" name="concern" />
+            </div>
+
+                <x-jet-button class="ml-4">
+                    {{ __('Book Appointment') }}
+                </x-jet-button>
+            </div>
+        </form>
+
+   
